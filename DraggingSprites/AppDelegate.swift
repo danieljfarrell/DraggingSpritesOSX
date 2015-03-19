2498928ffcde7b942f744254505e48fa196f6c6e
daniel j farrell <daniel.james.farrell@gmail.com>
2015-03-19 12:42:49 +0000

    Initial Commit

---
+++ DraggingSprites/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DraggingSprites
+//
+//  Created by Daniel Farrell on 19/03/2015.
+//  Copyright (c) 2015 Daniel Farrell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
